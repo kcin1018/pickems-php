7996ab976bed6d8b6aab5911df04c3240ddd3f8b
Nick Felicelli <nicholas.felicelli@cchmc.org>
2016-04-25 10:32:35 -0400

    Still working on config for heroku

--- config/config.exs
+++ config/config.exs
@@ -7,31 +7,17 @@ use Mix.Config
 
 # Configures the endpoint
 config :pickems, Pickems.Endpoint,
-  http: [port: {:system, "PORT"}],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "pickems-fb-api.herokuapp.com", port: 443],
+  url: [host: "localhost"],
   root: Path.dirname(__DIR__),
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  secret_key_base: "Gy3uafdaGg4ohhDRew3RCoLpVSdbxsgmsEBv4qz2eNcKl4VnTlrdUBqHVNxhOSmv",
   render_errors: [accepts: ~w(json)],
   pubsub: [name: Pickems.PubSub,
            adapter: Phoenix.PubSub.PG2]
-
-config :pickems, Pickems.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: 20
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
-
-config :phoenix, :format_encoders,
-  "json-api": Poison
-
-config :plug, :mimes, %{
-  "application/vnd.api+json" => ["json-api"]
-}
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
